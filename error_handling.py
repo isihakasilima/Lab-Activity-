5d0b23cdc7ad9fc5df80f8e23544b9fc7c79bd03
isihakasilima <isihakasilima1104@gmail.com>
2025-12-29 15:06:36 +0300

    add sample text to explain on script

--- error_handling.py
+++ error_handling.py
@@ -1,14 +1,19 @@
 def create_file(file_path):
-    with open(file_path, 'w') as file:
+    with open(file_path, "w") as file:
         file.write("This is a sample text file.\n")
-        file.write("It contains multiple lines of text.\n")
-        file.write("This is useful for testing file reading.\n")
+        file.write("Overview\n")
+        file.write("This script demonstrates basic file handling. It shows how to:\n")
+        file.write("Create file and write content to it.\n")
+        file.write("Read the content of the file.\n")
+        file.write("Ensure that file is properly closed using a finally block.\n")
+
     print(f"File '{file_path}' created successfully.")
+
 
 def read_file(file_path):
     file = None
     try:
-        file = open(file_path, 'r') 
+        file = open(file_path, "r")
         content = file.read()
         print(content)
     except FileNotFoundError:
@@ -22,8 +27,8 @@ def read_file(file_path):
             file.close()
             print("File has been closed.")
 
-file_path = 'sample.txt'
+
+file_path = "sample.txt"
 
 create_file(file_path)
-
 read_file(file_path)
